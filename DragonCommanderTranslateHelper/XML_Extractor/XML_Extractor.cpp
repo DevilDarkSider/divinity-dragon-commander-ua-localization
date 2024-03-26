a8e836f191a5704dca9b41d5eda319a330bccb30
Arsen Senkiv <senkivarsen@gmail.com>
2024-03-26 14:47:55 +0200

    Added code for extracting data from xmls

--- DragonCommanderTranslateHelper/XML_Extractor/XML_Extractor.cpp
+++ DragonCommanderTranslateHelper/XML_Extractor/XML_Extractor.cpp
@@ -3,13 +3,22 @@
 #include "windows.h"
 #include <fstream>
 #include <atlbase.h>
-#include <variant>
+#include <vector>
+#include <atlstr.h>
 
-LPCWSTR sPath = L"O:\\SteamLibrary\\steamapps\\common\\Divinity Dragon Commander\\Data\\Mods\\Main\\Gameplay\\Hints\\Hints.lsx";
+LPCWSTR sPath = LR"(D:\SteamLibrary\steamapps\common\Divinity Dragon Commander\Data\Mods\Main\Gameplay\Story\Dialogs\aida_1_dwarf_council.lsx)";
+
+struct XmlData
+{
+	CStringW sData;
+	CStringW sHandle;
+};
+
+std::vector<XmlData> aHints;
 
 int main()
 {
-	HRESULT hr = CoInitialize(nullptr);
+	/*HRESULT hr = CoInitialize(nullptr);
 	if (FAILED(hr))
 		MessageBox(nullptr, L"Failed to CoInitialize", L"Failed", MB_OK);
 
@@ -27,26 +36,126 @@ int main()
 	VARIANT_BOOL bSuc = VARIANT_FALSE;
 	hr = pXMLDoc->load(var, &bSuc);
 
-	VariantClear(&var);
 
 
-	CComBSTR sName;
-	pXMLDoc->get_nodeName(&sName);
-	std::wcout << sName.m_str << std::endl;
+	
 
 	CComPtr<IXMLDOMNodeList> pNodes;
 	pXMLDoc->get_childNodes(&pNodes);
 
+	CComPtr<IXMLDOMNode> pNode;
+	pNodes->get_item(1, &pNode);
+
+	pNodes = nullptr;
+	pNode->get_childNodes(&pNodes);
+
+	pNode = nullptr;
+	pNodes->get_item(2, &pNode);
+
+
+	pNodes = nullptr;
+	pNode->get_childNodes(&pNodes);
+
+	pNode = nullptr;
+	pNodes->get_item(0, &pNode);
+
+	pNodes = nullptr;
+	pNode->get_childNodes(&pNodes);
+
+	pNode = nullptr;
+	pNodes->get_item(0, &pNode);
+
+	pNodes = nullptr;
+	pNode->get_childNodes(&pNodes);
+
 	long nCount = 0;
 	pNodes->get_length(&nCount);
 
-	std::wcout << L"Count of nodes: " << nCount << std::endl;
-
 	for (long i = 0; i < nCount; i++)
 	{
-		CComPtr<IXMLDOMNode> pNode;
-		pNodes->get_item(i, &pNode);
-		pNode->get_nodeName(&sName);
-		std::wcout << sName.m_str << std::endl;
+		CComPtr<IXMLDOMNode> pItem;
+		pNodes->get_item(i, &pItem);
+
+		CComPtr<IXMLDOMNodeList> pItemNodes;
+		pItem->get_childNodes(&pItemNodes);
+
+		CComPtr<IXMLDOMNode> pItemAtributes;
+		pItemNodes->get_item(0, &pItemAtributes);
+
+		CComPtr<IXMLDOMNamedNodeMap> pMap;
+		pItemAtributes->get_attributes(&pMap);
+
+		VARIANT val;
+		VariantInit(&val);
+
+		CComPtr<IXMLDOMNode> pVal;
+		pMap->removeNamedItem(CComBSTR(L"value"), &pVal);
+		pVal->get_nodeValue(&val);
+
+		hintData data;
+		data.sData.SetString(val.bstrVal);
+
+		pVal = nullptr;
+		pMap->removeNamedItem(CComBSTR(L"handle"), &pVal);
+		pVal->get_nodeValue(&val);
+
+		data.sHandle.SetString(val.bstrVal);
+
+		aHints.push_back(std::move(data));
+
+		VariantClear(&val);
 	}
+
+	VariantClear(&var);*/
+
+	std::wifstream file(sPath, std::ios_base::in | std::ios_base::binary);
+	
+	std::wstring sLine;
+	wchar_t ch = 0;
+	while (file.get(ch))
+	{
+		if (ch == L'\n')
+		{
+			long nFind = sLine.find(L"<attribute id=\"Text\"");
+			if (nFind >= 0)
+			{
+				XmlData data;
+				
+				long nVal = sLine.find(L"value=");
+				nVal += 7;
+				long nEnd = sLine.find(L"\"", nVal);
+
+				data.sData.SetString(sLine.c_str() + nVal, nEnd - nVal);
+
+				nVal = sLine.find(L"handle=");
+				nVal += 8;
+				nEnd = sLine.find(L"\"", nVal);
+				data.sHandle.SetString(sLine.c_str() + nVal, nEnd - nVal);
+
+				aHints.push_back(std::move(data));
+			}
+			sLine.clear();
+			continue;
+		}
+
+		sLine.append(1, ch);
+	}
+	
+	
+	file.close();
+
+
+	CStringW path(sPath);
+	long nPos = path.ReverseFind(L'.');
+	path.Delete(nPos, 4);
+	path.Append(L".txt");
+
+	std::wfstream newFile(path, std::ios_base::out | std::ios_base::binary);
+
+	for (const auto& item : aHints)
+	{
+		newFile << L"\"" << item.sData.GetString() << L"\";" << item.sHandle.GetString() << std::endl;
+	}
+	newFile.close();
+
 }
